88ef7d7e09c89c322d114ba1f326ed72b76829fe
John Sangiolo <johnsangiolo@gmail.com>
2018-12-17 12:41:10 -0500

    initial server setup

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const parser = require('body-parser');
+
+const app = express();
+const port = 4000;
+
+app.use(parser.json());
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.get('/*', (req, res) => {
+  res.send('Hello, world!');
+})
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+})
